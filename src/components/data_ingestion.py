6dca9a93a3b208373663c6822373c97867d80b07
vignesh <vignesh2914@gmail.com>
2024-07-22 17:26:45 +0530

    Updated Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTraining
+from src.components.model_trainer import ModelTrainerconfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -47,12 +50,15 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
-        
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _= data_transformation.initiate_data_transformation(train_data, test_data)
 
+    model_trainer = ModelTraining()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
